cc79e0659342ff26afdd86588080a7915400bdbe
barrysweeney <barrysweeney172@live.co.uk>
2020-05-07 21:15:20 +0100

    iniital commit

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form");
+});
+
+router.post("/new", function (req, res, next) {
+  const text = req.body.text;
+  const user = req.body.user;
+  const added = new Date();
+  const message = { text: text, user: user, added: added };
+  messages.push(message);
+  res.redirect("/");
+});
+
+module.exports = router;
